34b0932ba651605ec7ecd3b6cb0e336b5b745ff7
Flavio Malavazi <flavio.malavazi1@gmail.com>
2023-08-24 16:31:37 -0300

    Adding demos folder to flake8 ignore

--- notebooks/Reading_a_Delta_Share.py
+++ notebooks/Reading_a_Delta_Share.py
@@ -2,7 +2,7 @@
 import delta_sharing
 
 # %%
-share_file_path = "../delta_sharing_credential_files/config_gcp.share" # for public shares: 'https://databricks-datasets-oregon.s3-us-west-2.amazonaws.com/delta-sharing/share/open-datasets.share'
+share_file_path = "../delta_sharing_credential_files/config_gcp_2.share" # for public shares: 'https://databricks-datasets-oregon.s3-us-west-2.amazonaws.com/delta-sharing/share/open-datasets.share'
 
 # Create a SharingClient
 client = delta_sharing.SharingClient(share_file_path)
@@ -21,7 +21,7 @@ for share in shares:
       print(f'name = {table.name}, share = {table.share}, schema = {table.schema}')
 
 # %%
-table_url = f"{share_file_path}#my_local_share.default.tab_job_demo"
+table_url = f"{share_file_path}#compartilhamento_azure.caio_demo_airbnb.listings_silver"
 
 # Use delta sharing client to load data
 pandas_df = delta_sharing.load_as_pandas(table_url)
