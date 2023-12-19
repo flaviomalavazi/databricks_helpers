5f34e629ca9227b1c90d1694375345450ff97e48
Flavio Malavazi <flavio.malavazi1@gmail.com>
2023-12-19 18:55:46 -0300

    Fix data modelling to refer dbt constructs correctly

--- demos/dbt_demo/dbt_project/credit_cards_dbt_demo/models/marketing_results.sql
+++ demos/dbt_demo/dbt_project/credit_cards_dbt_demo/models/marketing_results.sql
@@ -10,7 +10,7 @@ select
     ,count(ma.media_payment_event_id)           as total_payments_found
     ,sum(ma.transaction_bill_value)             as total_payments_value
 from 
-    flavio_malavazi.dbt_credit_cards_demo.consolidated_media as cm
-    left join flavio_malavazi.dbt_credit_cards_demo.marketing_attribution as ma on ma.media_attributed_first_ad = cm.ad_id and cm.investment_interval = date_trunc("hour", ma.transaction_timestamp)
+    {{ ref('consolidated_media') }} as cm
+    left join {{ ref('marketing_attribution') }} as ma on ma.media_attributed_first_ad = cm.ad_id and cm.investment_interval = date_trunc("hour", ma.transaction_timestamp)
 group by
     all
